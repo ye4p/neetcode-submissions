abe59a31c1b67493ffd17db1ae28fe8ce540144c
Danila <danilgolubkov172@gmail.com>
2026-06-02 12:52:59 -0400

    Add: binary-search - submission-11

---
+++ Data Structures & Algorithms/binary-search/submission-11.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int low = 0;
+        int high=nums.size()-1;
+        int mid;
+        while (high >= low) {
+            mid =(high+low)/2;
+            if (nums[mid]==target) return mid;
+            if (nums[mid]>target) high=mid-1;
+            if (nums[mid]<target) low = mid+1;
+        }
+        return -1;
+    }
+};
