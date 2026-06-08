70282fa786d5ece056259eaf9e396206ec6b34ae
Danila <danilgolubkov172@gmail.com>
2026-06-08 10:34:55 -0400

    Add: valid-sudoku - submission-0

---
+++ Data Structures & Algorithms/valid-sudoku/submission-0.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        int rows[9] = {0};
+        int cols[9] = {0};
+        int squares[9] = {0};
+
+        for (int r = 0; r < 9; ++r) {
+            for (int c = 0; c < 9; ++c) {
+                if (board[r][c] == '.') continue;
+
+                int val = board[r][c] - '1';
+
+                if ((rows[r] & (1 << val)) || (cols[c] & (1 << val)) ||
+                    (squares[(r / 3) * 3 + (c / 3)] & (1 << val))) {
+                    return false;
+                }
+
+                rows[r] |= (1 << val);
+                cols[c] |= (1 << val);
+                squares[(r / 3) * 3 + (c / 3)] |= (1 << val);
+            }
+        }
+        return true;
+    }
+};
